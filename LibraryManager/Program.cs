c2a3bbe590a891c0ea6288d5d45695e0b7af9cd4
Akvir03 <nathan.devienne.pro@gmail.com>
2024-01-29 09:57:14 +0100

    Ajout de la class Program & de sa méthode main

--- LibraryManager/Program.cs
+++ LibraryManager/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    
+    private static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                // Configuration des services
+            });
+    }
+
+    public static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        // Exécution du host
+        host.Run();
+    }
+}
+
