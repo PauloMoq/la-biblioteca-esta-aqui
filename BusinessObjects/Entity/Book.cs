7bf5e3258cf9f1654a8b5a14eadc5c5af070b572
Akvir03 <nathan.devienne.pro@gmail.com>
2024-01-29 10:50:16 +0100

    Ajout des références

--- BusinessObjects/Entity/Book.cs
+++ BusinessObjects/Entity/Book.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BusinessObjects.Entity
 {
-    internal class Book
+    public class Book
     {
         public int? Id { get; set; }
         public string? Name { get; set; }
--- DataAccessLayer/Repository/BookRepository.cs
+++ DataAccessLayer/Repository/BookRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessObjects.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repository
 {
-    internal class BookRepository
+    public class BookRepository
     {
         public IEnumerable<Book> GetAll()
         {
